21717b54977001e5a027a3ca612b3c57a9c08eea
Stephen Fabian <fabianstephen@gmail.com>
2022-07-27 10:58:08 -0600

    add reccommend_exibits method

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,9 +1,10 @@
 class Museum
-    attr_reader :name, :exhibits
+    attr_reader :name, :exhibits, :patrons
  
     def initialize(name)
         @name = name
         @exhibits = []
+        @patrons = []
     end
 
     def add_exhibit(exhibit)
@@ -18,4 +19,35 @@ class Museum
         end
         recommended_exhibits.sort
     end
+
+    def admit(patron)
+        @patrons << patron
+    end
+
+
+    def patrons_by_exhibit_interest
+
+        exhibit_names = []
+        @exhibits.each do |exhibit|
+            exhibit_names << exhibit.name
+        end
+
+        patron_interest_summary = exhibit_names.group_by { |exhibit_name| exhibit_name}
+
+        patrons.each do |patron|
+            if patron.interests.include?("Gems and Minersals")
+            patron_interest_summary[:"Gems and Minersals"] = patron.name
+            if patron.interests.include?("Dead Sea Scrolls")
+                patron_interest_summary[:"Dead Sea Scrolls"] = patron.nam
+            if patron.interests.include?("imax")
+            patron_interest_summary[:"imax"] = patron
+            end
+            end
+            end
+        end
+    
+
+       
+    end
+
 end
